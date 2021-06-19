a486ce24c9c4295184d310be3d9759d71e135fbc
Edazpotato <edazpotato@gmail.com>
2021-06-19 16:08:38 +1200

    Initial commit

---
+++ src/main/java/codes/edaz/customwandsplugin/CustomWandsPlugin.java
@@ -0,0 +1,17 @@
+package codes.edaz.customwandsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomWandsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
